3db9cbf906a1a91addfa998a3d75956344b8654b
andrewlfauth <andrewlfauth@gmail.com>
2023-01-19 15:43:38 -0800

    rename and refactor

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import Card from './components/Card.vue'
 import CutView from './components/CutView/CutView.vue'
-import HandView from './components/HandView/HandView.vue'
+import CribSelectionView from './components/CribSelectionView/CribSelectionView.vue'
 import StartView from './components/StartView/StartView.vue'
 import { useGameStore } from './stores/game'
 
@@ -9,8 +9,9 @@ const { game } = useGameStore()
 </script>
 <template>
   <main :class="game.theme" class="h-screen overflow-hidden bg-gray-900">
-    <StartView v-if="!game.stage" />
+    <!-- <StartView v-if="!game.stage" />
     <CutView v-if="game.stage == 'cut'" />
-    <HandView v-if="game.stage == 'deal'" />
+    <CribSelectionView v-if="game.stage == 'deal'" /> -->
+    <CribSelectionView v-if="true" />
   </main>
 </template>
