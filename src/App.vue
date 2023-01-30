5fb12e26d467cafa0078f07a4d3deac350fb3abb
andrewlfauth <andrewlfauth@gmail.com>
2023-01-29 23:48:31 -0800

    min viewport width for app 768px

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,10 @@ const { game } = useGameStore()
 </script>
 <template>
   <GameOver v-if="game.winner" />
-  <main :class="game.theme" class="h-screen overflow-hidden bg-gray-900">
+  <main
+    :class="game.theme"
+    class="h-screen overflow-hidden bg-gray-900 hidden md:block"
+  >
     <StartView v-if="!game.stage" />
     <CutView v-if="game.stage == 'cut'" />
     <CribSelectionView v-if="game.stage == 'deal'" />
@@ -22,4 +25,16 @@ const { game } = useGameStore()
     <ScoreBoard />
     <Toast />
   </main>
+  <div
+    class="md:hidden bg-gray-900 w-screen h-screen flex flex-col items-center justify-center text-gray-400 text-center"
+  >
+    <span class="text-3xl">Tablet or Desktop only :(</span>
+    <a
+      class="mt-4 underline"
+      href="https://github.com/andrewlfauth/cribbage"
+      target="_blank"
+      ref="'noreferrer'"
+      >Find out why</a
+    >
+  </div>
 </template>
